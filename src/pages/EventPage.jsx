df3487a543a3ce69a2d71aae1c7cdaeb070c3c5f
Lehlogonolo <lehlogonolotsidiso@gmail.com>
2026-01-06 16:33:14 +0200

    Protect acess to  event page when logged out

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,3 +1,20 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { SessionState } from "../sharedstate/SessionState";
+
 export default function EventPage() {
-  return <h1>Add / Edit Event</h1>;
+  const navigate = useNavigate();
+  const { loggedIn } = useContext(SessionState);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login");
+    }
+  }, [loggedIn, navigate]);
+
+  return (
+    <div className="page-container">
+      <h1 className="page-heading">Add Event</h1>
+    </div>
+  );
 }
